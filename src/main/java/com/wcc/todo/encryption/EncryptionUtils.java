93d596329188af5d5f0dce83882b8707cf1b1004
WangChuncheng <chuncheng.wang@hand-china.com>
2018-12-07 15:14:54 +0800

    简单的密码加密和验证

--- src/main/java/com/wcc/todo/encryption/EncryptionUtils.java
+++ src/main/java/com/wcc/todo/encryption/EncryptionUtils.java
@@ -1,13 +1,27 @@
 package com.wcc.todo.encryption;
 
 public class EncryptionUtils {
-    public static String encrypt(String plaintext){
-        // TODO
-        return null;
+    private static final int cipherlen = 30;//密文长度
+    private static final char increasingVal = 57;
+    private static final int offset = 19;
+    public static String encrypt(String plaintext) {
+        char[] sourceChars = plaintext.toCharArray();
+        char[] targetChars = new char[30];
+        int len = sourceChars.length;
+        int displacement = (len + offset)%len;
+        for (int i = 0; i < len; i ++) {
+            char c = sourceChars[i];
+            targetChars[(i+displacement)%len] = (char) ((c + increasingVal)%(128));
+        }
+        /* todo fill to cipherlen
+        for (int i = len; i < cipherlen; i++) {
+            targetChars[i] = (char) ((i * increasingVal + )%128);
+        }*/
+        return new String(targetChars);
     }
 
-    public static String decrypt(String ciphertext){
-        // TODO
-        return null;
+    public static boolean validate(String plaintext,String ciphertext) {
+        String encrypted = encrypt(plaintext);
+        return encrypted.equals(ciphertext);
     }
 }
--- src/test/java/com/wcc/todo/EncryptionTester.java
+++ src/test/java/com/wcc/todo/EncryptionTester.java
@@ -1,4 +1,27 @@
 package com.wcc.todo;
 
+import com.wcc.todo.encryption.EncryptionUtils;
+import org.junit.Test;
+
 public class EncryptionTester {
+    private String generateString(int len) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            char c = (char) ((Math.random() * 1000) % 128);
+            builder.append(c);
+        }
+        return builder.toString();
+    }
+
+    @Test
+    public void testEncryption() {
+        String old;
+        String encrypted;
+        for (int i = 0; i < 100; i++) {
+            old = generateString(8);
+            encrypted = EncryptionUtils.encrypt(old);
+            boolean same = EncryptionUtils.validate(old,encrypted);
+            System.out.println("same = " + same);
+        }
+    }
 }
