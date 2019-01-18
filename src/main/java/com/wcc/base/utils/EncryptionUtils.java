14af1f687b772ab36b5041d8fd8c7c9c89de3619
WangChuncheng <chuncheng.wang@hand-china.com>
2019-01-18 17:12:03 +0800

    修复逻辑错误

--- src/main/java/com/wcc/base/utils/EncryptionUtils.java
+++ src/main/java/com/wcc/base/utils/EncryptionUtils.java
@@ -1,35 +1,40 @@
 package com.wcc.base.utils;
 
+
 import com.wcc.base.exception.BusinessException;
 
+import javax.validation.constraints.NotNull;
+
+/**
+ * 简单的加密
+ * @author wangchuncheng
+ */
 public class EncryptionUtils {
-    private static final int cipherlen = 30;//密文长度
-    private static final char increasingVal = 57;
-    private static final int offset = 19;
+    /** 密文长度 */
+    private static final int CIPHER_LEN = 30;
+    private static final char INCREASING_VAL = 57;
+    private static final int OFFSET = 19;
+
+    private EncryptionUtils() {
+    }
 
     public static String encrypt(String plaintext) throws BusinessException {
-        if (plaintext.length() > cipherlen) {
+        if (plaintext.length() > CIPHER_LEN) {
             throw new BusinessException("密码超过最大长度！");
         }
         char[] sourceChars = plaintext.toCharArray();
         char[] targetChars = new char[30];
         int len = sourceChars.length;
-        int displacement = (len + offset) % len;
+        int displacement = (len + OFFSET) % len;
         for (int i = 0; i < len; i++) {
             char c = sourceChars[i];
-            targetChars[(i + displacement) % len] = (char) ((c + increasingVal) % (128));
-        }
-        for (int i = len; i < cipherlen; i++) {
-            targetChars[i] = (char) (Math.random() * 128);
+            targetChars[(i + displacement) % len] = (char) ((c + INCREASING_VAL) % (128));
         }
         return new String(targetChars);
     }
 
-    public static boolean validate(String plaintext, String ciphertext) throws BusinessException {
-        int plaintextLen = plaintext.length();
-        String encrypted = encrypt(plaintext);
-        String source = encrypted.substring(0, plaintextLen);
-        String target = ciphertext.substring(0, plaintextLen);
-        return source.equals(target);
+    public static boolean validate(@NotNull String plaintext, String ciphertext) throws BusinessException {
+        String source = encrypt(plaintext);
+        return source.equals(ciphertext);
     }
 }
